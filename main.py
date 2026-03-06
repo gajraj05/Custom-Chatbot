fd2bb47892c31e3f860362e098d298b252b956f1
Gajraj Singh Rathore <118117459+gajraj05@users.noreply.github.com>
2026-03-06 13:57:33 +0530

    Refactor main.py and add home endpoint
    Removed commented-out code and added a home endpoint.

--- main.py
+++ main.py
@@ -1,62 +1,21 @@
-# import logging
-# from fastapi import FastAPI
-# import inngest
-# import inngest.fast_api
-# from inngest.experimental import ai
-# from dotenv import load_dotenv
-# import os
-# import uuid
-# import datetime
-
-
-
-# load_dotenv()
-
-# inngest_client = inngest.Inngest(
-#     app_id="rag_app",
-#     logger=logging.getLogger("unicorn"),
-#     is_production=False,
-#     serializer=inngest.PydanticSerializer(),
-# )
-
-# @inngest_client.create_function(
-#     fn_id="RAG: Ingest PDF",
-#     trigger=inngest.TriggerEvent(event="rag/ingest_pdf"),
-# )
-
-# async def rag_ingest_pdf(ctx: inngest.Context):
-#     return {"Hello": "World"}
-
-
-# app = FastAPI()
-
-# inngest_app = inngest.fast_api.serve(app, inngest_client, functions=[rag_ingest_pdf])
-
-# # if __name__ == "__main__":
-# #     main()
-
-# main.py
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from rag_chatbot.chatbot_model import ask_chatbot
 
-print("Starting FastAPI server...")
-
 app = FastAPI()
 
-# Request body
 class Query(BaseModel):
     question: str
 
-# API Endpoint
+@app.get("/")
+def home():
+    return {"message": "Portfolio Chatbot API Running"}
+
 @app.post("/chat")
 def chat(query: Query):
-    from rag_chatbot.chatbot_model import ask_chatbot
     answer = ask_chatbot(query.question)
     return {"answer": answer}
 
-
-# Run server
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
